5fa9f0c06c656a82e65041b7f4dbdddb803329db
Jose Quintana <joseluisquintana20@gmail.com>
2020-04-09 01:51:14 +0200

    feat: initial gotham implementation

--- src/main.rs
+++ src/main.rs
@@ -1,88 +1,50 @@
-extern crate chrono;
-extern crate env_logger;
-extern crate flate2;
-extern crate hyper_native_tls;
-extern crate iron;
-extern crate iron_staticfile_middleware;
-extern crate nix;
-extern crate signal;
-
-#[macro_use]
-extern crate log;
-extern crate structopt;
-
-mod config;
-mod error_page;
-mod gzip;
-mod logger;
-mod signal_manager;
-mod staticfiles;
-
-use crate::config::Options;
-use chrono::Local;
-use env_logger::Builder;
-use hyper_native_tls::NativeTlsServer;
-use iron::prelude::*;
-use log::LevelFilter;
-use staticfiles::*;
-use std::io::Write;
+use gotham::handler::assets::FileOptions;
+use gotham::middleware::logger::SimpleLogger;
+use gotham::pipeline::new_pipeline;
+use gotham::pipeline::single::single_pipeline;
+use gotham::router::builder::*;
+use gotham::router::builder::{DefineSingleRoute, DrawRoutes};
+use gotham::router::Router;
+use log::Level;
 use structopt::StructOpt;
 
-fn on_server_running(server_name: &str, proto: &str, addr: &str) {
-    // Notify when server is running
-    info!(
-        "Static {} Server `{}` is running on {}",
-        proto, server_name, addr
-    );
+mod config;
 
-    // Wait for incoming signals (E.g Ctrl+C (SIGINT), SIGTERM, etc
-    signal_manager::wait_for_signal(|sig: signal::Signal| {
-        let code = signal_manager::signal_to_int(sig);
+use crate::config::Options;
 
-        println!();
-        warn!("SIGINT {} caught. HTTP Server execution exited.", code);
-        std::process::exit(code)
+fn router(opts: &Options) -> Router {
+    let index_file = format!("{}/index.html", opts.root);
+
+    let (chain, pipelines) =
+        single_pipeline(new_pipeline().add(SimpleLogger::new(Level::Info)).build());
+
+    build_router(chain, pipelines, |route| {
+        route.associate("/", |assoc| {
+            assoc.head().to_file(&index_file);
+            assoc.get().to_file(index_file);
+        });
+        route.associate("/*", |assoc| {
+            assoc.head().to_dir(
+                FileOptions::new(&opts.root)
+                    .with_cache_control("no-cache")
+                    .with_gzip(true)
+                    .build(),
+            );
+            assoc.get().to_dir(
+                FileOptions::new(&opts.root)
+                    .with_cache_control("no-cache")
+                    .with_gzip(true)
+                    .build(),
+            );
+        });
     })
 }
 
-fn main() {
-    Builder::new()
-        .format(|buf, record| {
-            writeln!(
-                buf,
-                "{} [{}] - {}",
-                Local::now().format("%Y-%m-%dT%H:%M:%S"),
-                record.level(),
-                record.args()
-            )
-        })
-        .filter(None, LevelFilter::Info)
-        .init();
+pub fn main() {
+    env_logger::init();
 
     let opts = Options::from_args();
-    let addr = &format!("{}{}{}", opts.host.to_string(), ":", opts.port.to_string());
-    let proto = if opts.tls { "HTTPS" } else { "HTTP" };
+    let addr = format!("{}{}{}", opts.host.to_string(), ":", opts.port.to_string());
 
-    // Configure & launch the HTTP server
-
-    let files = StaticFiles::new(StaticFilesOptions {
-        root_dir: opts.root,
-        assets_dir: opts.assets,
-        page_50x_path: opts.page50x,
-        page_404_path: opts.page404,
-    });
-
-    if opts.tls {
-        let ssl = NativeTlsServer::new(opts.tls_pkcs12, &opts.tls_pkcs12_passwd).unwrap();
-
-        match Iron::new(files.handle()).https(addr, ssl) {
-            Result::Ok(_) => on_server_running(&opts.name, &proto, addr),
-            Result::Err(err) => panic!("{:?}", err),
-        }
-    } else {
-        match Iron::new(files.handle()).http(addr) {
-            Result::Ok(_) => on_server_running(&opts.name, &proto, addr),
-            Result::Err(err) => panic!("{:?}", err),
-        }
-    }
+    gotham::start(addr, router(&opts))
 }
