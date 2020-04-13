cafcae872bdf6aeff9b6158ab462c751790be7c3
Jose Quintana <joseluisquintana20@gmail.com>
2020-04-14 00:57:20 +0200

    refactor: more tests for main file

--- src/main.rs
+++ src/main.rs
@@ -24,14 +24,40 @@ pub fn main() {
 
 #[cfg(test)]
 mod tests {
-    extern crate http;
-
     use super::*;
     use gotham::test::TestServer;
     use http::StatusCode;
 
     #[test]
-    fn receive_router_response() {
+    fn receive_ok_head_response() {
+        let opts = Options::from_args();
+        let router = RouterHandler::new(opts);
+        let test_server = TestServer::new(router.handle()).unwrap();
+        let response = test_server
+            .client()
+            .head("http://localhost")
+            .perform()
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+    }
+
+    #[test]
+    fn receive_404_head_response() {
+        let opts = Options::from_args();
+        let router = RouterHandler::new(opts);
+        let test_server = TestServer::new(router.handle()).unwrap();
+        let response = test_server
+            .client()
+            .head("http://localhost/dummy")
+            .perform()
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::NOT_FOUND);
+    }
+
+    #[test]
+    fn receive_ok_get_response() {
         let opts = Options::from_args();
         let router = RouterHandler::new(opts);
         let test_server = TestServer::new(router.handle()).unwrap();
@@ -45,7 +71,7 @@ mod tests {
     }
 
     #[test]
-    fn receive_404_response() {
+    fn receive_404_get_response() {
         let opts = Options::from_args();
         let router = RouterHandler::new(opts);
         let test_server = TestServer::new(router.handle()).unwrap();
@@ -56,5 +82,19 @@ mod tests {
             .unwrap();
 
         assert_eq!(response.status(), StatusCode::NOT_FOUND);
+    }
+
+    #[test]
+    fn receive_405_delete_response() {
+        let opts = Options::from_args();
+        let router = RouterHandler::new(opts);
+        let test_server = TestServer::new(router.handle()).unwrap();
+        let response = test_server
+            .client()
+            .delete("http://localhost")
+            .perform()
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::METHOD_NOT_ALLOWED);
     }
 }
